37aaa41ea68ee7da5f78bd3af390090ab779407b
LeagueLugas <leaguelugas@gmail.com>
2020-06-17 14:41:36 +0900

    [HIT] 이성진

--- src/main/java/kr/hs/dsm_scarfs/service/evaluation/EvaluationServiceImpl.java
+++ src/main/java/kr/hs/dsm_scarfs/service/evaluation/EvaluationServiceImpl.java
@@ -1,22 +1,16 @@
 package kr.hs.dsm_scarfs.service.evaluation;
 
-import kr.hs.dsm_scarfs.domain.entitys.EvaluationMutual;
-import kr.hs.dsm_scarfs.domain.entitys.EvaluationSelf;
-import kr.hs.dsm_scarfs.domain.entitys.Homework;
-import kr.hs.dsm_scarfs.domain.entitys.User;
+import kr.hs.dsm_scarfs.domain.entitys.*;
 import kr.hs.dsm_scarfs.domain.payload.request.EvaluationMutualRequest;
 import kr.hs.dsm_scarfs.domain.payload.request.EvaluationSelfRequest;
-import kr.hs.dsm_scarfs.domain.repository.EvaluationMutualRepository;
-import kr.hs.dsm_scarfs.domain.repository.EvaluationSelfRepository;
-import kr.hs.dsm_scarfs.domain.repository.HomeworkRepository;
-import kr.hs.dsm_scarfs.domain.repository.UserRepository;
-import kr.hs.dsm_scarfs.exception.AlreadyEvaluationException;
-import kr.hs.dsm_scarfs.exception.HomeworkNotFoundException;
-import kr.hs.dsm_scarfs.exception.TargetNotFoundException;
-import kr.hs.dsm_scarfs.exception.UserNotFoundException;
+import kr.hs.dsm_scarfs.domain.repository.*;
+import kr.hs.dsm_scarfs.exception.*;
 import kr.hs.dsm_scarfs.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EvaluationServiceImpl implements EvaluationService {
@@ -29,6 +23,10 @@ public class EvaluationServiceImpl implements EvaluationService {
     private EvaluationSelfRepository evaluationSelfRepository;
     @Autowired
     private EvaluationMutualRepository evaluationMutualRepository;
+    @Autowired
+    private TeamRepository teamRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Override
     public void evaluationSelf(String token, EvaluationSelfRequest evaluationSelfRequest) {
@@ -67,6 +65,18 @@ public class EvaluationServiceImpl implements EvaluationService {
                 .ifPresent(evaluationMutual -> {
                     throw new AlreadyEvaluationException();
                 });
+
+        if (user.getId().equals(target.getId())) {
+            List<Team> teams = teamRepository.findByHomeworkId(evaluationMutualRequest.getHomeworkId()).orElseGet(ArrayList::new);
+            List<Integer> teamIds = new ArrayList<>();
+            for (Team t : teams) teamIds.add(t.getId());
+
+            Team targetTeam = teamRepository.findById(
+                    memberRepository.findByUserIdAndTeamIdIn(user.getId(), teamIds).orElseThrow(MemberNotFoundException::new).getTeamId()
+            ).orElseThrow(TeamNotFoundException::new);
+
+            target = userRepository.findById(targetTeam.getLeaderId()).orElseThrow(UserNotFoundException::new);
+        }
 
         evaluationMutualRepository.save(
                 EvaluationMutual.builder()
